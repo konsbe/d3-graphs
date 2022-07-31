b95dc6a605f1f3392cfda9fc1b23c8e61030b5a0
kberdelis <konstantinos.berdelis@gugroup.com>
2022-07-31 18:30:42 +0300

    its easy ! nothing can be made it can be made

--- src/components/BubbleChart/index.tsx
+++ src/components/BubbleChart/index.tsx
@@ -4,13 +4,22 @@ import { scaleLinear } from "d3-scale";
 // import { event } from "d3";
 import { axisBottom, axisLeft } from "d3-axis";
 import "d3-transition";
+import { easeElastic, easeExpInOut } from "d3-ease";
+
 import * as d3 from "d3";
 import { initialData } from "./countries";
+import { countriesOTW } from "./countriesOTW";
 import "./BubbleChart.css";
+
 const BubbleChart = (): JSX.Element => {
   const dimensions = { width: 1200, height: 550 };
   const svgRef = useRef<SVGSVGElement | null>(null);
-  const [data, setData] = useState(initialData);
+  // const [data, setData] = useState<Array<{}>>(initialData);
+  const [data, setData] = useState<Array<{}>>(countriesOTW);
+  // const [setOption, option] = useState<string>("initialData");
+  // const [dataX, setDataX] = useState<string | number>("");
+  // const [dataY, setDataY] = useState<string | number>("");
+  // const [dataZ, setDataZ] = useState<string | number>("");
   const tooltip = select("#tooltip").style("opacity", 0);
 
   // csv = "https://raw.githubusercontent.com/holtzy/data_to_viz/master/Example_dataset/4_ThreeNum.csv"
@@ -29,13 +38,6 @@ const BubbleChart = (): JSX.Element => {
         "transform",
         `translate(calc(-90px + ${d.clientX}px), calc(${d.clientY}px))`
       );
-    // selection
-    //   .append("g")
-    //   .append("rect")
-    //   .attr("class", "dotted")
-    //   .attr("stroke-width", "1px")
-    //   .attr("width", ".5px")
-    //   .attr("height", dimensions.height - 1000);
   };
   var moveTooltip = function (d: any) {
     tooltip.style(
@@ -49,7 +51,6 @@ const BubbleChart = (): JSX.Element => {
   let x = scaleLinear().domain([0, 10000]).range([0, width]);
   // Add Y axis
   let y = scaleLinear().domain([35, 90]).range([dimensions.height, 0]);
-  // svg.append("g").call(d3.axisLeft(y));
 
   // Add a scale for bubble size
   var z = d3.scaleLinear().domain([200000, 1310000000]).range([1, 40]);
@@ -72,32 +73,35 @@ const BubbleChart = (): JSX.Element => {
         .enter()
         .append("circle")
         .attr("class", "bubbles")
-        .attr("cx", function (d: any) {
-          return x(d.gdpPercap);
-        })
-        .attr("cy", function (d: any) {
-          return y(d.lifeExp);
-        })
-        .attr("r", function (d: any) {
-          return z(Number(d.pop) * 2);
-        })
-        .attr("fill", function (d: any): any {
-          if (d.continent === "Asia") {
-            return "#2DD7EB";
-          } else if (d.continent === "Europe") {
-            return "#3CEB2D";
-          } else if (d.continent === "Americas") {
-            return "#F55431";
-          } else if (d.continent === "Africa") {
-            return "#F33485";
-          } else if (d.continent === "Oceania") {
-            return "#Fff931";
-          }
+        .style("opacity", "0.7")
+        .attr("cx", 0)
+        .attr("cy", 0)
+        .attr("r", 0)
+        .attr("fill", function (d: any) {
+          return colorizedContentents(d);
         })
         .on("mouseover", (d) => showTooltip(d, selection))
         .on("mousemove", moveTooltip)
         .on("mouseout", hideTooltip)
-        .style("opacity", "0.7")
+        .transition()
+        .ease(easeExpInOut)
+        .delay((_, i) => i * 10)
+        .duration(500)
+        .attr("cx", function (d: any) {
+          let xData: number = d.gdpPercap
+            ? d.gdpPercap
+            : d.areaSqMi / 100 + 200;
+          return x(xData);
+        })
+        .attr("cy", function (d: any) {
+          let yData: number = d.lifeExp ? d.lifeExp : d.birthrate * 2;
+          return y(yData);
+        })
+        .attr("r", function (d: any) {
+          let zData: number = d.pop ? Number(d.pop) * 2 : d.population * 10;
+          return z(zData);
+        })
+        // .style("opacity", "0.7")
         .attr("stroke", "black");
     }
     // const margin = { top: 20, right: 30, bottom: 30, left: 30 };
@@ -112,19 +116,45 @@ const BubbleChart = (): JSX.Element => {
     svg.append("g").call(axisLeft(y));
   }, [selection]);
 
+  const changeData = (x: number) => {
+    if (x === 1) {
+      setData(countriesOTW);
+    } else {
+      setData(initialData);
+    }
+  };
+
   return (
-    <div style={{ marginLeft: "100px" }}>
-      <div id="tooltip" className="tooltip" style={{ width: "15%" }}>
-        <div className="tooltip-title">
-          <span id="title" />
+    <>
+      <div style={{ marginLeft: "100px" }}>
+        <div id="tooltip" className="tooltip" style={{ width: "15%" }}>
+          <div className="tooltip-title">
+            <span id="title" />
+          </div>
+          <div className="tooltip-value" style={{ backgroundColor: "black" }}>
+            <span id="count" style={{ color: "white", margin: "1rem" }} />
+          </div>
         </div>
-        <div className="tooltip-value" style={{ backgroundColor: "black" }}>
-          <span id="count" style={{ color: "white", margin: "1rem" }} />
-        </div>
+        <svg ref={svgRef} width={dimensions.width} height={dimensions.height} />
       </div>
-      <svg ref={svgRef} width={dimensions.width} height={dimensions.height} />
-    </div>
+      <button onClick={() => changeData(1)}>Change 1</button>
+      <button onClick={() => changeData(2)}>Change 2</button>
+    </>
   );
 };
 
 export default BubbleChart;
+
+export const colorizedContentents = (d: any): any => {
+  if (d.continent === "Asia") {
+    return "#2DD7EB";
+  } else if (d.continent === "Europe") {
+    return "#3CEB2D";
+  } else if (d.continent === "Americas") {
+    return "#F55431";
+  } else if (d.continent === "Africa") {
+    return "#F33485";
+  } else if (d.continent === "Oceania") {
+    return "#Fff931";
+  }
+};
