d11aca659048c04a63a1b2641922c0ebf30ff108
kberdelis <konstantinos.berdelis@gugroup.com>
2022-07-31 22:09:44 +0300

    another pv

--- src/components/BubbleChart/index.tsx
+++ src/components/BubbleChart/index.tsx
@@ -4,6 +4,7 @@ import { scaleLinear } from "d3-scale";
 // import { event } from "d3";
 import { axisBottom, axisLeft } from "d3-axis";
 import "d3-transition";
+import { interpolate } from "d3-interpolate";
 import { easeElastic, easeExpInOut } from "d3-ease";
 
 import * as d3 from "d3";
@@ -14,12 +15,9 @@ import "./BubbleChart.css";
 const BubbleChart = (): JSX.Element => {
   const dimensions = { width: 1200, height: 550 };
   const svgRef = useRef<SVGSVGElement | null>(null);
-  // const [data, setData] = useState<Array<{}>>(initialData);
-  const [data, setData] = useState<Array<{}>>(countriesOTW);
-  // const [setOption, option] = useState<string>("initialData");
-  // const [dataX, setDataX] = useState<string | number>("");
-  // const [dataY, setDataY] = useState<string | number>("");
-  // const [dataZ, setDataZ] = useState<string | number>("");
+  const [data, setData] = useState<Array<{}>>(initialData);
+  // const [data, setData] = useState<Array<{}>>(countriesOTW);
+  const [option, setOption] = useState<string>("initialData");
   const tooltip = select("#tooltip").style("opacity", 0);
 
   // csv = "https://raw.githubusercontent.com/holtzy/data_to_viz/master/Example_dataset/4_ThreeNum.csv"
@@ -30,10 +28,14 @@ const BubbleChart = (): JSX.Element => {
 
   var showTooltip = function (d: any, selection: any) {
     tooltip.transition().duration(200);
+    console.log(d.target.__data__);
+    let string: string = d.target.__data__.population
+      ? d.target.__data__.country + ": " + d.target.__data__?.population
+      : d.target.__data__.country + ": " + d.target.__data__.lifeExp;
     tooltip
       .style("opacity", 1)
       .select("#count")
-      .text(d.path[0].__data__.country + ": " + d.path[0].__data__.gdpPercap)
+      .text(string)
       .style(
         "transform",
         `translate(calc(-90px + ${d.clientX}px), calc(${d.clientY}px))`
@@ -50,10 +52,10 @@ const BubbleChart = (): JSX.Element => {
   };
   let x = scaleLinear().domain([0, 10000]).range([0, width]);
   // Add Y axis
-  let y = scaleLinear().domain([35, 90]).range([dimensions.height, 0]);
+  let y = scaleLinear().domain([0, 90]).range([dimensions.height, 0]);
 
   // Add a scale for bubble size
-  var z = d3.scaleLinear().domain([200000, 1310000000]).range([1, 40]);
+  let z = d3.scaleLinear().domain([200000, 1310000000]).range([1, 40]);
 
   const [selection, setSelection] = useState<null | Selection<
     SVGSVGElement | null,
@@ -85,8 +87,8 @@ const BubbleChart = (): JSX.Element => {
         .on("mouseout", hideTooltip)
         .transition()
         .ease(easeExpInOut)
-        .delay((_, i) => i * 10)
-        .duration(500)
+        .delay((_, i) => i)
+        .duration(1000)
         .attr("cx", function (d: any) {
           let xData: number = d.gdpPercap
             ? d.gdpPercap
@@ -115,12 +117,70 @@ const BubbleChart = (): JSX.Element => {
 
     svg.append("g").call(axisLeft(y));
   }, [selection]);
+  useEffect(() => {
+    if (selection) {
+      const circles = selection.selectAll(".bubbles").data(data);
+      circles
+        .transition()
+        .delay((_, i) => i)
+        .duration(1000)
+        .attr("fill", function (d: any) {
+          return colorizedContentents(d);
+        })
+        // .ease(easeElastic)
+        .attr("cx", function (d: any) {
+          let xData: number =
+            option === "initialData"
+              ? d.gdpPercap
+              : option === "countriesOTW"
+              ? d.areaSqMi / 100 + 200
+              : d.cropsPerc * 1000 + 200
+              ? d.cropsPerc * 1000 + 200
+              : 0;
+
+          return x(xData);
+        })
+        .attr("cy", function (d: any) {
+          let yData: number =
+            option === "initialData"
+              ? d.lifeExp
+              : option === "countriesOTW"
+              ? d.birthrate * 2
+              : d.deathrate * 3
+              ? d.deathrate * 3
+              : 0;
+          return y(yData);
+        })
+        .attr("r", function (d: any) {
+          let zData: number =
+            option === "initialData"
+              ? Number(d.pop) * 2
+              : option === "countriesOTW"
+              ? d.population * 10
+              : d.industry * 1000000000
+              ? d.industry * 1000000000
+              : 0;
+          return z(zData);
+        })
+
+        // .style("opacity", "0.7")
+        .attr("stroke", "black");
+    }
+  }, [option]);
 
   const changeData = (x: number) => {
     if (x === 1) {
+      console.log(x);
       setData(countriesOTW);
-    } else {
+      setOption("countriesOTW");
+    } else if (x === 2) {
+      console.log(x);
       setData(initialData);
+      setOption("initialData");
+    } else if (x === 3) {
+      console.log(x);
+      setData(countriesOTW);
+      setOption("countriesFTW");
     }
   };
 
@@ -139,6 +199,7 @@ const BubbleChart = (): JSX.Element => {
       </div>
       <button onClick={() => changeData(1)}>Change 1</button>
       <button onClick={() => changeData(2)}>Change 2</button>
+      <button onClick={() => changeData(3)}>Change 3</button>
     </>
   );
 };
@@ -146,15 +207,30 @@ const BubbleChart = (): JSX.Element => {
 export default BubbleChart;
 
 export const colorizedContentents = (d: any): any => {
-  if (d.continent === "Asia") {
-    return "#2DD7EB";
-  } else if (d.continent === "Europe") {
-    return "#3CEB2D";
-  } else if (d.continent === "Americas") {
-    return "#F55431";
-  } else if (d.continent === "Africa") {
-    return "#F33485";
-  } else if (d.continent === "Oceania") {
-    return "#Fff931";
+  if (d.continent) {
+    if (d.continent === "Asia") {
+      return "#2DD7EB";
+    } else if (d.continent === "Europe") {
+      return "#3CEB2D";
+    } else if (d.continent === "Americas") {
+      return "#F55431";
+    } else if (d.continent === "Africa") {
+      return "#F33485";
+    } else if (d.continent === "Oceania") {
+      return "#Fff931";
+    }
+  } else {
+    // console.log(d.region.includes("ASIA"));
+    if (d.region?.includes("ASIA")) {
+      return "#2DD7EB";
+    } else if (d.region?.includes("EURO")) {
+      return "#3CEB2D";
+    } else if (d.region?.includes("AMER")) {
+      return "#F55431";
+    } else if (d.region?.includes("AFRI")) {
+      return "#F33485";
+    } else if (d.region?.includes("OCEANI")) {
+      return "#Fff931";
+    }
   }
 };
